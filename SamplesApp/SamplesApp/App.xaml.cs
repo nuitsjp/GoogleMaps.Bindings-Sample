5e8e32a0731b99245ba9fab52bf74cdb89ba77c5
Atsushi Nakamura <nuits.jp@live.jp>
2017-09-13 23:22:10 +0900

    Add Sample.

---
+++ SamplesApp/SamplesApp/App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using SamplesApp.Views;
+using Xamarin.Forms;
+
+namespace SamplesApp
+{
+    public partial class App
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new MenuPage());
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
---
+++ SamplesApp/SamplesApp/ViewModels/MoveToRegionPageViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows.Input;
+using Xamarin.Forms;
+using Xamarin.Forms.GoogleMaps;
+using Xamarin.Forms.GoogleMaps.Bindings;
+
+namespace SamplesApp.ViewModels
+{
+    public class MoveToRegionPageViewModel
+    {
+        public MoveToRegionRequest MoveToRegionRequest { get; } = new MoveToRegionRequest();
+
+        public ICommand MoveToTokyoCommand => new Command(() =>
+        {
+            MoveToRegionRequest.MoveToRegion(
+                MapSpan.FromCenterAndRadius(
+                    new Position(35.681298, 139.766247),
+                    Distance.FromKilometers(2)));
+        });
+    }
+}
